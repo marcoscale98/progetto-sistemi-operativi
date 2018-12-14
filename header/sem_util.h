d85f30eb46ea2ff5fd4bcbb161afcb5dcff927b7
Davide Zago <zago.davide@educ.di.unito.it>
2018-12-15 00:36:14 +0100

    Ho tolto la definizione di funzioni da sem_util.h

--- header/sem_util.h
+++ header/sem_util.h
@@ -1,8 +1,5 @@
-#ifndef SEM_UTIL
-#define SEM_UTIL
-
-#include <sys/types.h>
-#include <sys/sem.h>
+#ifndef _SEM_UTIL_
+#define _SEM_UTIL_
 
 union semun {
     // value for SETVAL
@@ -19,31 +16,15 @@ union semun {
 };
 
 // Initialize semaphore to 1 (i.e., "available")
-int initSemAvailable(int semId, int semNum) {
-   union semun arg;
-   arg.val = 1;
-   return semctl(semId, semNum, SETVAL, arg);
-}
+int initSemAvailable(int semId, int semNum);
+
 // Initialize semaphore to 0 (i.e., "in use")
-int initSemInUse(int semId, int semNum) {
-   union semun arg;
-   arg.val = 0;
-   return semctl(semId, semNum, SETVAL, arg);
-}
+int initSemInUse(int semId, int semNum);
 
 // Reserve semaphore - decrement it by 1
-int reserveSem(int semId, int semNum) {
-   struct sembuf sops;
-   sops.sem_num = semNum;
-   sops.sem_op = -1;
-   sops.sem_flg = 0;
-   return semop(semId, &sops, 1);
-}
+int reserveSem(int semId, int semNum);
+
 // Release semaphore - increment it by 1
-int releaseSem(int semId, int semNum) {
-   struct sembuf sops;
-   sops.sem_num = semNum;
-   sops.sem_op = 1;
-   sops.sem_flg = 0;
-   return semop(semId, &sops, 1);
-}
+int releaseSem(int semId, int semNum);
+
+#endif
