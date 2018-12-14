5fdf3c67394610f69ff07b3acfeea83b5f027cf5
Davide Zago <zago.davide@educ.di.unito.it>
2018-12-14 19:26:30 +0100

    Upload New File

---
+++ header/error.h
@@ -0,0 +1,12 @@
+#ifndef _ERROR_H_
+#define _ERROR_H_
+
+#define TEST_ERROR    if (errno) {fprintf(stderr, \
+                       "%s:%d: PID=%5d: Error %d (%s)\n",\
+                       __FILE__,\
+                       __LINE__,\
+                       getpid(),\
+                       errno,\
+                       strerror(errno)); exit(EXIT_FAILURE);}
+
+#endif
