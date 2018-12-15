d9600f22c2fc17fb7340d61aa67d76dd76940b3e
marcoscale98 <scalerandi.marco@educ.di.unito.it>
2018-12-15 13:53:47 +0100

    Aggiunti include mancanti

--- header/error.h
+++ header/error.h
@@ -1,6 +1,11 @@
 #ifndef _ERROR_H_
 #define _ERROR_H_
 
+#include <errno.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <stdlib.h>
+
 #define TEST_ERROR    if (errno) {fprintf(stderr, \
                        "%s:%d: PID=%5d: Error %d (%s)\n",\
                        __FILE__,\
