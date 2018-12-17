68cfb3f8a8acc53f8110c97110717989888d06d7
Davide Zago <zago.davide@educ.di.unito.it>
2018-12-17 12:31:00 +0100

    Update conf_reader.c

--- conf_reader.c
+++ conf_reader.c
@@ -5,20 +5,13 @@
 #include "header/error.h"
 #define POPSIZE 10
 
-struct student{
-    int nof_elems;
-    int max_rejects;
-    int nof_invites;
-};
-
 void init_student(char*,struct student*);
 
-int main(){
+/*int main(){
     struct student stud;
     init_student("opt.conf",&stud);
-    printf("nof_elems: %d\nmax_rejects: %d\nnof_invites: %d\n",stud.nof_elems, stud.max_rejects, stud.nof_invites);
-
-}
+    printf("nof_elems: %d\nmax_reject: %d\nnof_invites: %d\n",stud.nof_elems, stud.max_reject, stud.nof_invites);
+}*/
 
 void init_student(char* path, struct student* stud){
     FILE* fs = fopen(path, "r");
@@ -28,28 +21,25 @@ void init_student(char* path, struct student* stud){
 
     //legge chiavi e valori
     while(fscanf(fs,"%s %d",key,&value)!=EOF){
-        if(!strcmp(key,"max_rejects"))
-            stud->max_rejects = value;
+        if(!strcmp(key,"max_reject"))
+            stud->max_reject = value;
         else if(!strcmp(key,"nof_invites"))
             stud->nof_invites = value;
         else if(!strcmp(key,"prob_2"))
-            prob_2 = value/100;
+            prob_2 = value/100.0;
         else if(!strcmp(key,"prob_3"))
-            prob_3 = value/100;
+            prob_3 = value/100.0;
         else if(!strcmp(key,"prob_4"))
-            prob_4 = value/100;
+            prob_4 = value/100.0;
     }
-
     //inizializzazione nof_elems
     srand(getpid());
     int val = rand()%POPSIZE;
-    printf("Val: %d\n", val);
     if(val<POPSIZE*prob_2)
         stud->nof_elems = 2;
-    else if(val<POPSIZE*prob_3)
+    else if(val>=POPSIZE*prob_2 && val<POPSIZE*(prob_2+prob_3))
         stud->nof_elems = 3;
-    else if(val<POPSIZE*prob_4)
+    else //if(val>=POPSIZE*(prob_2+prob_3) && val<POPSIZE)
         stud->nof_elems = 4;
-
     fclose(fs);
 }
