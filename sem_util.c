407bedccdc7ea5ca08c44ace51c3bef0e3889aca
Davide Zago <zago.davide@educ.di.unito.it>
2018-12-18 22:05:31 +0100

    Ho corretto i nomi con underscore e minuscole

--- sem_util.c
+++ sem_util.c
@@ -3,41 +3,40 @@
 #include "header/sem_util.h"
 
 // Inizializzazione del semaforo a valore inserito da utente
-int initSem(int semId, int semNum, int semVal) {
+int init_sem(int semid, int semnum, int semval) {
    union semun arg;
-   arg.val = semVal;
-   return semctl(semId, semNum, SETVAL, arg);
+   arg.val = semval;
+   return semctl(semid, semnum, SETVAL, arg);
 }
 
-
 // Inizializzazione del semaforo a 1 (i.e., "available")
-int initSemAvailable(int semId, int semNum) {
+int init_sem_available(int semid, int semnum) {
    union semun arg;
    arg.val = 1;
-   return semctl(semId, semNum, SETVAL, arg);
+   return semctl(semid, semnum, SETVAL, arg);
 }
 
 // Inizializzazione del semaforo a 0 (i.e., "in use")
-int initSemInUse(int semId, int semNum) {
+int init_sem_in_use(int semid, int semnum) {
    union semun arg;
    arg.val = 0;
-   return semctl(semId, semNum, SETVAL, arg);
+   return semctl(semid, semnum, SETVAL, arg);
 }
 
 // Reserve semaphore - decremento di 1
-int reserveSem(int semId, int semNum) {
+int reserve_sem(int semid, int semnum) {
    struct sembuf sops;
-   sops.sem_num = semNum;
+   sops.sem_num = semnum;
    sops.sem_op = -1;
    sops.sem_flg = 0;
-   return semop(semId, &sops, 1);
+   return semop(semid, &sops, 1);
 }
 
 // Release semaphore - incremento di 1
-int releaseSem(int semId, int semNum) {
+int release_sem(int semid, int semnum) {
    struct sembuf sops;
-   sops.sem_num = semNum;
+   sops.sem_num = semnum;
    sops.sem_op = 1;
    sops.sem_flg = 0;
-   return semop(semId, &sops, 1);
+   return semop(semid, &sops, 1);
 }
