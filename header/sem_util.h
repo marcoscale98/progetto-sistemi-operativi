ec7336877cb5af50b4513bf793232362dd723120
Davide Zago <zago.davide@educ.di.unito.it>
2018-12-18 22:07:09 +0100

    corretto i nomi con underscore e minuscole

--- header/sem_util.h
+++ header/sem_util.h
@@ -1,32 +1,34 @@
-#ifndef _SEM_UTIL_
-#define _SEM_UTIL_
+#ifndef SEM_UTIL
+#define SEM_UTIL
+
+#include <sys/types.h>
+#include <sys/sem.h>
+
+//definizione visuale dei semafori ready, per la memoria condivisa di info_student e gruppo
+#define SEM_READY 0
+#define SEM_SHM_STUD 1
+#define SEM_SHM_GRUPPO 2
 
 union semun {
-    // value for SETVAL
-    int val;
-    // buffer for IPC_STAT, IPC_SET
-    struct semid_ds* buf;
-    // array for GETALL, SETALL
-    unsigned short* array;
-    // Linux specific part
-   #if defined(__linux__)
-    // buffer for IPC_INFO
-    struct seminfo* __buf;
-   #endif
+    int val;                    // value for SETVAL
+    struct semid_ds* buf;       // buffer for IPC_STAT, IPC_SET
+    unsigned short* array;      // array for GETALL, SETALL
+    #if defined(__linux__)
+    struct seminfo* __buf;      // buffer for IPC_INFO
+    #endif
 };
-// Inizializzazione semaforo a valore inserito da utente
-int initSem(int semId, int semNum, int semVal);
+
+// Inizializzazione del semaforo a valore inserito da utente
+int init_sem(int semid, int semnum, int semval);
 
 // Inizializzazione del semaforo a 1 (i.e., "available")
-int initSemAvailable(int semId, int semNum);
+int init_sem_available(int semid, int semnum);
 
-// Inizializzazione del semaforo a 1 0 (i.e., "in use")
-int initSemInUse(int semId, int semNum);
+// Inizializzazione del semaforo a 0 (i.e., "in use")
+int init_sem_in_use(int semid, int semnum);
 
 // Reserve semaphore - decremento di 1
-int reserveSem(int semId, int semNum);
+int reserve_sem(int semid, int semnum);
 
 // Release semaphore - incremento di 1
-int releaseSem(int semId, int semNum);
-
-#endif
+int release_sem(int semid, int semnum);
