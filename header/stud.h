d37c9f1b27ce72066ab15a40fa93d48f155ef284
marcoscale98 <scalerandi.marco@educ.di.unito.it>
2018-12-19 21:26:18 +0100

    Update stud.h

--- header/stud.h
+++ header/stud.h
@@ -8,9 +8,11 @@ struct info_student{
     int group;     //gruppo di cui lo studente fa parte
 };
 
-struct gruppo{
-    int members;  //numero di elementi del gruppo
-    int is_closed;  //0 se e' chiuso, 1 alrimenti
+struct info_group{
+    int n_members;  //numero di elementi del gruppo
+    int is_closed;  //0 false (aperto), 1 true (chiuso)
+    int pref_nof_elems;     //moda del valore nof_elems dei membri
+    int max_voto;   //voto massimo del gruppo
 };
 
 struct student{
@@ -18,5 +20,7 @@ struct student{
     int max_reject;
     int nof_invites;
 };
+
+// struct info_group gruppo[POP_SIZE];
 
 #endif
