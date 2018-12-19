b770f21c010785f77a9ef21ed857eda47f31c17a
Davide Zago <zago.davide@educ.di.unito.it>
2018-12-19 16:54:40 +0100

    Update stud.h

--- stud.h
+++ stud.h
@@ -6,11 +6,15 @@ struct info_student{
     int nof_elems;  //nof_elems dello studente
     int voto_AdE;   //voto_AdE dello studente
     int group;     //gruppo di cui lo studente fa parte
-}
+};
 
-struct gruppo{
-    int members;  //numero di elementi del gruppo
+struct info_group{
+    int n_members;  //numero di elementi del gruppo
     int is_closed;  //0 se e' chiuso, 1 alrimenti
-}
+    int pref_nof_elems;     //moda del valore nof_elems dei membri
+    int max_voto;   //voto massimo del gruppo
+};
+
+// struct info_group gruppo[POP_SIZE];
 
 #endif
