9ceaf65ccb8e04b40118d4b12fb9ec688661abc8
marcoscale98 <scalerandi.marco@educ.di.unito.it>
2018-12-20 12:02:53 +0100

    Aggiunto shmid e qualche correzione

--- student.c
+++ student.c
@@ -5,49 +5,53 @@
 
 #define NOGROUP 0
 
-int main(int argc, char *argv[]){ /*argv[0]="student", argv[1]=matricola, argv[2]=prob_2, argv[3]=prob_3,
-    argv[4]=nof_invites, argv[5]=max_reject, argv[6]=POPSIZE */
+int main(int argc, char *argv[]){ /*argv[0]="student", argv[1]=matricola, argv[2]=prob_2, argv[3]=prob_3, argv[4]=nof_invites, argv[5]=max_reject, argv[6]=POPSIZE, argv[7]=shmid */
     
-    if(argc!=7){
+    if(argc!=8){
         printf("Numero di argomenti inseriti non corretto\n");
         exit(EXIT_FAILURE);
     }
     
     //definizione e inizializzazione variabili studente    
-    struct info_student *stud;
-    float prob_2= atof(argv[2]),
-        prob_3= atof(argv[3]);
-    int nof_invites = atoi(argv[4]),
-        max_reject = atoi(argv[5]),
-        popsize = atoi(argv[6]);
-    
-    stud->matricola = atoi(argv[1]);
-    stud->group = NOGROUP; //non ancora in un gruppo
+    struct info_student info_stud;
+    float prob_2 = atoi(argv[2])/100.0,
+          prob_3 = atoi(argv[3])/100.0;
+    int popsize = atoi(argv[6]),
+        shmid = atoi(argv[7]),
+        nof_invites = atoi(argv[4]),
+        max_reject = atoi(argv[5]);
+        
+    info_stud.matricola = atoi(argv[1]);
+    info_stud.group = NOGROUP; //non ancora in un gruppo
     //l'indice dei gruppi parte dall'1
     
     //inizializzazione voto_AdE
     srand(getpid());
-    stud->voto_AdE = rand()%13 + 18;  //compreso tra 18 e 30
+    info_stud.voto_AdE = rand()%13 + 18;  //compreso tra 18 e 30
 
     //inizializzazione nof_elems
     srand(getpid());
     int val = rand()%popsize;
     if(val<popsize*prob_2)
-        stud->nof_elems = 2;
+        info_stud.nof_elems = 2;
     else if(val>=popsize*prob_2 && val<popsize*(prob_2+prob_3))
-        stud->nof_elems = 3;
+        info_stud.nof_elems = 3;
     else //if(val>=POPSIZE*(prob_2+prob_3) && val<POPSIZE)
-        stud->nof_elems = 4;
+        info_stud.nof_elems = 4;
     
-    printf(" stud->matricola: %d\n", stud->matricola);
+    //TESTING
+    /*
+    printf(" info_stud.matricola: %d\n", info_stud.matricola);
     printf(" prob_2: %f\n", prob_2);
     printf(" prob_3: %f\n", prob_3);
     printf(" nof_invites: %d\n", nof_invites);
     printf(" max_reject: %d\n", max_reject);
     printf(" popsize: %d\n", popsize);
-    printf(" stud->group: %d\n", stud->group);
-    printf(" stud->voto_AdE: %d\n", stud->voto_AdE);
-    printf(" stud->nof_elems: %d\n", stud->nof_elems);
+    printf(" info_stud.group: %d\n", info_stud.group);
+    printf(" info_stud.voto_AdE: %d\n", info_stud.voto_AdE);
+    printf(" info_stud.nof_elems: %d\n", info_stud.nof_elems);
+    printf(" shmid: %d\n", shmid);
+    */
         
     //incremento del semaforo di 1
     
