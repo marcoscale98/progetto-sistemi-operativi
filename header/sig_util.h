c1df2dad4be79ae612d7a7afc42fcbaa9dd2c90f
Davide Zago <zago.davide@educ.di.unito.it>
2018-12-26 12:05:23 +0100

    Upload New File

---
+++ header/sig_util.h
@@ -0,0 +1,16 @@
+#ifndef _SIG_UTIL_H_
+#define _SIG_UTIL_H_
+
+//handler per SIGINT: rimuove le strutture ipc e poi uccide il process group
+void handler_sigint(int sig);
+
+//handler per SIGALRM: allo scadere del timer sospende i processi studente
+void handler_sigalrm(int sig);
+
+//funzione richiamata per impostare l'handler di SIGINT
+int sa_sigint();
+
+//funzione richiamata per impostare l'handler di SIGALRM
+int sa_sigalrm();
+
+#endif
