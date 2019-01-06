4b6129c81abbebee4e4c30c03ef19c6335d21046
marcoscale98 <scalerandi.marco@educ.di.unito.it>
2019-01-06 10:52:31 +0100

    Modifiche

--- header/stud.h
+++ header/stud.h
@@ -1,37 +1,45 @@
 #ifndef _STUD_H_
 #define _STUD_H_
 
-void accetta_invito(int mittente , int destinatario , int msg_id);
+#define MSG_LEN 100
 
-int chiudo_gruppo();
-
-//controlla gli inviti ricevuti e li valuta
-//return true se accetta un invito
-//return false se non accetta
-int rispondo_inviti(int msg_id, int *accettato);
+struct msgbuf{
+   int mtype;             /* message type, sarà > 0 */
+   char text[MSG_LEN];    /* message testo */
+};
 
 //controlla se ha ricevuto risposta agli inviti
 //return true se tutti hanno risposto
 //return false se qualcuno non ha risposto
-int controllo_risposte(int msg_id, int *invitati);
+int controllo_risposte(int *invitati);
 
-//controlla che tutti gli invitati abbiano risposto agli inviti
-int hanno_risposto(int *invitati);
+//controlla gli inviti ricevuti e li valuta
+//return true se accetta un invito
+//return false se non accetta
+int rispondo_inviti(int *accettato, int *n_rifiutati, int max_reject) ;
 
-void inserisci_nel_mio_gruppo(int matricola);
+int max(int num1, int num2);
 
-void invita_studente(int *invitati, int mittente, int destinatario, int msg_id);
-
-void mando_inviti(int msg_id, int *invitati) ;
-
-void rifiuta_invito(int mittente, int destinatario, int msg_id);
-
-//funzione che imposta a -1 l'elemento che contiene la matricola uguale a mittente
-void setta_risposta(int *invitati,int mittente);
+void mando_inviti(int *invitati, int *n_invitati, int nof_invites);
 
 //confronta le matricole di 2 studenti e verifica siano nello stesso turno
 int stesso_turno (struct info_student *mat1, struct info_student *mat2);
 
-int max(int num1, int num2);
+//decido se conviene chiudere il gruppo(return true) oppure no(return false)
+int chiudo_gruppo();
+
+void inserisci_nel_mio_gruppo(int matricola);
+
+void invita_studente(int destinatario, int *invitati, int *n_invitati);
+
+void rifiuta_invito(int mittente, int *n_rifiutati);
+
+void accetta_invito(int mittente);
+
+//controlla che tutti gli invitati abbiano risposto agli inviti
+int hanno_risposto(int *invitati);
+
+//funzione che imposta a -1 l'elemento che contiene la matricola uguale a mittente
+void setta_risposta(int mittente, int *invitati);
 
 #endif
