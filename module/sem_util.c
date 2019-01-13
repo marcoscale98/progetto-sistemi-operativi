bd9c58130634c6b4963c8aa17bf888a5ac09376b
marcoscale98 <scalerandi.marco@educ.di.unito.it>
2019-01-13 10:35:38 +0100

    Carica un nuovo file

---
+++ module/sem_util.c
@@ -0,0 +1,54 @@
+#include <sys/types.h>
+#include <sys/sem.h>
+#include "../header/sem_util.h"
+#include "../header/error.h"
+
+// Ritorna il valore del semaforo (considerando anche valori negativi)
+int get_sem_val(int semid, int semnum) {
+    union semun arg;
+    int semncnt = semctl(semid, semnum, GETNCNT, arg);
+    TEST_ERROR;
+    if(semncnt > 0)
+        return -semncnt;
+    else
+        return semctl(semid, semnum, GETVAL, arg);
+}
+
+// Inizializzazione del semaforo a valore inserito da utente
+int init_sem(int semid, int semnum, int semval) {
+   union semun arg;
+   arg.val = semval;
+   return semctl(semid, semnum, SETVAL, arg);
+}
+
+// Inizializzazione del semaforo a 1 (i.e., "available")
+int init_sem_available(int semid, int semnum) {
+   union semun arg;
+   arg.val = 1;
+   return semctl(semid, semnum, SETVAL, arg);
+}
+
+// Inizializzazione del semaforo a 0 (i.e., "in use")
+int init_sem_in_use(int semid, int semnum) {
+   union semun arg;
+   arg.val = 0;
+   return semctl(semid, semnum, SETVAL, arg);
+}
+
+// Reserve semaphore - decremento di 1
+int reserve_sem(int semid, int semnum) {
+   struct sembuf sops;
+   sops.sem_num = semnum;
+   sops.sem_op = -1;
+   sops.sem_flg = 0;
+   return semop(semid, &sops, 1);
+}
+
+// Release semaphore - incremento di 1
+int release_sem(int semid, int semnum) {
+   struct sembuf sops;
+   sops.sem_num = semnum;
+   sops.sem_op = 1;
+   sops.sem_flg = 0;
+   return semop(semid, &sops, 1);
+}
