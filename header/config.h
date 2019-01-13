8165de2d7ff87ddadc0f5e241c415fcea96a264f
marcoscale98 <scalerandi.marco@educ.di.unito.it>
2019-01-13 10:33:34 +0100

    Carica un nuovo file

---
+++ header/config.h
@@ -0,0 +1,17 @@
+#ifndef _CONFIG_H_
+#define _CONFIG_H_
+
+#define POP_SIZE 30
+#define TRUE 1
+#define FALSE 0
+#define ARG_SIZE 32
+#define IPC_KEY ftok("opt.conf",0)
+#define CRITIC_TIME (0.5*POP_SIZE)
+
+//definire soltanto se si vuole che si stampino dei check
+#define DEBUG
+
+//definire soltanto se si vuole che con una TEST_ERROR si esca in caso di errore
+//#define EXIT_ON_ERROR
+
+#endif
